b36b30a73648f676c4eb36454e5cf1ea84ca90f4
IsraelGalvez <galvez.rli.51@gmail.com>
2023-03-07 21:35:48 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,60 @@
  **/
 
 console.log('Happy hacking :)')
+
+const baseUrl = 'https://platzi-avo.vercel.app'
+
+// En el HTML cree un div con id 'app'
+const appNote = document.querySelector('.app')
+
+// Api Intl
+// 1 - formato a fechas
+// 2 - formato monedas
+const formatPrice = (price) => {
+
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(price)
+
+    return newPrice
+}
+
+//web api
+//conectarnos al servidor
+window
+.fetch(`${baseUrl}/api/avo`)
+//Procesar la respueta, y convertirla en JSON
+.then(respuesta => respuesta.json())
+//JSON -> DATA -> Renderizar info browser
+.then((responseJson) => {
+    const todasLaImagenes = []
+    responseJson.data.forEach((item) => {
+        //Crear imagen
+        const imagen = document.createElement('img')
+        imagen.src = `${baseUrl}/${item.image}`
+        imagen.className = "imageAvocado"
+
+        //Crear titulo
+        const title = document.createElement('h2')
+        title.textContent = item.name
+        title.className = "avocadoName"
+
+        //Crear precio
+        const price = document.createElement('div')
+        price.textContent = formatPrice(item.price)
+        price.className = "avocadoPrice"
+
+        const containerNamePrice = document.createElement('div')
+        containerNamePrice.append(title, price)
+        containerNamePrice.className = "containerNamePrice"
+
+        const container = document.createElement('div')
+        container.append(imagen, containerNamePrice)
+        container.className = "avocadoContainer"
+
+        todasLaImagenes.push(container)
+    })
+
+    appNote.append(...todasLaImagenes)
+})
